71de924988abbad9a6133f12f59800eddc1585a9
NishiGaba <gabanishi45@gmail.com>
2018-01-31 16:27:00 +0530

    Image SCrapping Done through Search Engine

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,28 @@
 var myApp = angular.module('imageApp', []);
 
-myApp.controller('imageController', ['$scope',function($scope) {
+myApp.controller('imageController', ['$scope','$http',function($scope, $http) {
 
-	(function() {
-  var cx = '014789268996448848800:t8sinqq1kjw'; // Insert your own Custom Search engine ID here
-  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-})();
+
+	//GETTING IMAGES OUTPUT THROUGH GOOGLE SEARCH ENGINE
+
+ 	(function() {
+	  var cx = '014789268996448848800:t8sinqq1kjw'; // Insert your own Custom Search engine ID here
+	  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+	  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx+'&searchType=image&imgcolor=black';
+	  var s = document.getElementsByTagName('script')[0];
+	  console.log(s.parentNode);
+	  s.parentNode.insertBefore(gcse, s);
+
+	  var checking = document.getElementsByTagName("img");
+	  console.log(checking);
+
+	  // $http.get('www.google.com/cse?cx=014789268996448848800:t8sinqq1kjw&q='++'&output=xml',function(response) {
+	  // 	console.log(response);
+	  // });
+	})();
 
 	$scope.imageSearch = function() {
 		console.log($scope.image.key);
-
-     
-
-
-// <script>
-//   (function() {
-//     var cx = '014789268996448848800:t8sinqq1kjw';
-//     var gcse = document.createElement('script');
-//     gcse.type = 'text/javascript';
-//     gcse.async = true;
-//     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-//     var s = document.getElementsByTagName('script')[0];
-//     s.parentNode.insertBefore(gcse, s);
-//   })();
-// </script>
-// <gcse:search></gcse:search>
 
 	};
 }])
--- routes/images/index.js
+++ routes/images/index.js
@@ -8,6 +8,8 @@ var Image = require('../../models/image');
 console.log('it comes here');
 
 //REST API CALLS
+
+//GETTING IMAGES
 router.get('/', function(req,res) {
 	Image.find(function(err,images) {
 		console.log(images);
@@ -15,6 +17,7 @@ router.get('/', function(req,res) {
 	});
 }); 
 
+//SAVE IMAGES
 router.post('/', function(req,res) {
 	console.log('image post');
 	let newImage = new Image({
@@ -31,10 +34,12 @@ router.post('/', function(req,res) {
 	})
 }); 
 
+//UPDATE IMAGES
 router.put('/:img_id', function(req,res) {
 	console.log('Image put!!!');
 }); 
 
+//DELETE IMAGES
 router.delete('/:img_id', function(req,res) {
 	console.log('Image delete!!!');
 
